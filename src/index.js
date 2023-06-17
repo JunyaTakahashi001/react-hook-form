e0a62fe644abf06f4c73974448ff668b52bc3774
junya.takahashi <junya.takahashi.001@gmail.com>
2023-06-18 00:12:40 +0900

    Yupを利用したバリデーション

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import App2 from './App2';
+import Yup from './Yup';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <App />
     <App2 />
+    <Yup />
   </React.StrictMode>
 );
 
